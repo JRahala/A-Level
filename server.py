b72fc726ac63725ac480a16add30df1e538b6eba
Jasamrit Singh Rahala <45700318+JRahala@users.noreply.github.com>
2022-01-20 23:03:33 +0000

    registration UI working

--- server.py
+++ server.py
@@ -1,7 +1,11 @@
-from operator import truediv
 from flask import Flask
-from flask import render_template
+from flask import render_template, redirect, url_for
+from flask_socketio import SocketIO, emit, send
+
+from users import *
+
 app = Flask(__name__)
+socketio = SocketIO(app)
 
 @app.route("/")
 def index():
@@ -11,5 +15,26 @@ def index():
 def register_login():
     return render_template("register_login.html")
 
-app.run(host = "0.0.0.0", port = 8080, debug = True)
+# handle user registration
+@socketio.on("register_user")
+def register_user(data):
+    print("ASDFASDFASDF")
+    # extract data from socket event    
+    email = data["email"]
+    username = data["username"]
+    password = data["password"]
+    confirmPassword = data["confirmPassword"]
+    # did registration work
+    registration_succeeded, new_user_object = User.register(email, username, password)
+    # emit event if failed
+    if (registration_succeeded): 
+        emit("registration_failed", {"details": "email already used"})
+    else:
+        emit("registration_failed", {"details": "email already used"})
+
+@app.route("/homepage/")
+def homepage():
+    return render_template("homepage.html")
+
+socketio.run(app, host = "0.0.0.0", port = 8080, debug = True)
 
